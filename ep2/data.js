8689b46572a8ebd47639c07466d87a842727e14e
benjamin-abk <78903534+benjamin-abk@users.noreply.github.com>
2021-02-20 08:56:56 +0100

    Add files via upload

---
+++ ep2/data.js
@@ -0,0 +1,596 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-start",
+      "name": "Start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.07624554974379194,
+        "pitch": -0.06663171459877226,
+        "fov": 1.5485404792658644
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.061935688464558325,
+          "pitch": -0.6033961890543953,
+          "rotation": 0,
+          "target": "1-brcke-1"
+        },
+        {
+          "yaw": 1.6440277984187999,
+          "pitch": 0.05873504234610749,
+          "rotation": 0,
+          "target": "10-gemeinsamer-platz---achse"
+        },
+        {
+          "yaw": 3.1140617079297055,
+          "pitch": 0.063034156879759,
+          "rotation": 0,
+          "target": "11-blick-gemeinsamer-platz"
+        },
+        {
+          "yaw": -0.028722605171768834,
+          "pitch": 0.064714357765979,
+          "rotation": 0,
+          "target": "8-zwischenraum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0500364357636887,
+          "pitch": 0.3225844932971498,
+          "title": "gemeinsamer Platz<br>",
+          "text": "Zwischen den Gebäuden der Achse ist dieser gemeinsame Platz verortet<br>"
+        },
+        {
+          "yaw": -1.440847500010932,
+          "pitch": 0.053165891098368334,
+          "title": "Zugang Unterführung<br>",
+          "text": "Abgang zur U-Bahn-Station Rathaus und zur Unterführung Richtung Mobility Hub<br>"
+        },
+        {
+          "yaw": 1.0236396686764486,
+          "pitch": -0.5639337757845588,
+          "title": "Haus der Bürger<br>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-brcke-1",
+      "name": "Brücke 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.1179675053032163,
+        "pitch": 0.26715186499561483,
+        "fov": 1.5485404792658644
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20677447135670235,
+          "pitch": 0.09635157566449415,
+          "rotation": 0,
+          "target": "2-brcke-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-brcke-2",
+      "name": "Brücke 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.048934465009182304,
+        "pitch": 0.032042696661411796,
+        "fov": 1.5485404792658644
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6549970479137759,
+          "pitch": 0.15871970843717342,
+          "rotation": 0,
+          "target": "3-empfang--event-stadthaus"
+        },
+        {
+          "yaw": -0.19258399948523142,
+          "pitch": 0.6403052635397817,
+          "rotation": 0,
+          "target": "8-zwischenraum"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-empfang--event-stadthaus",
+      "name": "Empfang / Event Stadthaus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0069812901552381845,
+        "pitch": 0.008300416716885195,
+        "fov": 1.5485404792658644
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7060557528225164,
+          "pitch": -0.17330586425569905,
+          "rotation": 0,
+          "target": "4-kooperationsraum-stadthaus"
+        },
+        {
+          "yaw": 1.2990932887866453,
+          "pitch": 0.1246481382351643,
+          "rotation": 0,
+          "target": "6-ansicht-achse---hauptsttter-strae"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.02556273836884948,
+          "pitch": 0.2862286347575278,
+          "title": "Empfangs- &amp; Eventraum Stadthaus<br>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-kooperationsraum-stadthaus",
+      "name": "Kooperationsraum Stadthaus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8470195289141627,
+        "pitch": -0.12133418998189427,
+        "fov": 1.5485404792658644
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0528056961756604,
+          "pitch": 0.01887442449977428,
+          "rotation": 0,
+          "target": "5-dachterasse"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dachterasse",
+      "name": "Dachterasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0491847082205936,
+          "pitch": 0.41900305327282084,
+          "rotation": 0,
+          "target": "6-ansicht-achse---hauptsttter-strae"
+        },
+        {
+          "yaw": 0.18143729927659358,
+          "pitch": 0.07491567812751754,
+          "rotation": 0,
+          "target": "4-kooperationsraum-stadthaus"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ansicht-achse---hauptsttter-strae",
+      "name": "Ansicht Achse - Hauptstätter Straße",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6177354666076713,
+          "pitch": 0.050504763558837595,
+          "rotation": 0,
+          "target": "7-zugang-zwischenraum"
+        },
+        {
+          "yaw": -0.490634708812566,
+          "pitch": 0.08545899045843441,
+          "rotation": 0,
+          "target": "9-achse"
+        },
+        {
+          "yaw": 0.06040830407251008,
+          "pitch": -0.6202643069405944,
+          "rotation": 0,
+          "target": "5-dachterasse"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-zugang-zwischenraum",
+      "name": "Zugang Zwischenraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08879659965284148,
+          "pitch": 0.07510203760367062,
+          "rotation": 0,
+          "target": "8-zwischenraum"
+        },
+        {
+          "yaw": -1.3074516600898143,
+          "pitch": 0.0789033055184234,
+          "rotation": 0,
+          "target": "6-ansicht-achse---hauptsttter-strae"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-zwischenraum",
+      "name": "Zwischenraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14170289665084823,
+          "pitch": 0.06276744822136848,
+          "rotation": 0,
+          "target": "0-start"
+        },
+        {
+          "yaw": 0.11683265383380359,
+          "pitch": -0.35965195796352845,
+          "rotation": 0,
+          "target": "1-brcke-1"
+        },
+        {
+          "yaw": 3.041278923593632,
+          "pitch": 0.12470818080527124,
+          "rotation": 0,
+          "target": "7-zugang-zwischenraum"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-achse",
+      "name": "Achse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08344077597125654,
+          "pitch": 0.05639893107062477,
+          "rotation": 0,
+          "target": "10-gemeinsamer-platz---achse"
+        },
+        {
+          "yaw": 3.0620635113271817,
+          "pitch": 0.04089131712601102,
+          "rotation": 0,
+          "target": "6-ansicht-achse---hauptsttter-strae"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-gemeinsamer-platz---achse",
+      "name": "gemeinsamer Platz - Achse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.33485857061955215,
+        "pitch": -0.03960370555860493,
+        "fov": 1.5485404792658644
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5580675434400213,
+          "pitch": 0.04181835591986527,
+          "rotation": 0,
+          "target": "11-blick-gemeinsamer-platz"
+        },
+        {
+          "yaw": 1.0440450271021362,
+          "pitch": 0.0330366859424025,
+          "rotation": 0,
+          "target": "9-achse"
+        },
+        {
+          "yaw": -0.5824262554022948,
+          "pitch": 0.09419834314678255,
+          "rotation": 0,
+          "target": "0-start"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-blick-gemeinsamer-platz",
+      "name": "Blick gemeinsamer Platz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01886791400852772,
+          "pitch": 0.20744832544415814,
+          "rotation": 0,
+          "target": "0-start"
+        },
+        {
+          "yaw": -0.04313964748688903,
+          "pitch": -0.2226588389239783,
+          "rotation": 0,
+          "target": "1-brcke-1"
+        },
+        {
+          "yaw": 0.9675211757932445,
+          "pitch": 0.0794254645070751,
+          "rotation": 0,
+          "target": "10-gemeinsamer-platz---achse"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6594922051619285,
+          "pitch": -0.6920149423848407,
+          "title": "Dach<br>",
+          "text": "bietet witterungsgeschützte Aussenräume<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
